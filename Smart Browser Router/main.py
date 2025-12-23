1e8d4fb4ac3cfc9ba04557771fab4eea962933a9
Soroush Bazgir <s.bazgir@asiab.local>
2025-12-23 14:33:57 +0330

    Logger added to monitor info

--- Smart Browser Router/main.py
+++ Smart Browser Router/main.py
@@ -1,13 +1,17 @@
 import sys
 from router import choose_profile
 from launcher import open_in_chrome
+from logger import logger
 
 def main():
     if len(sys.argv) < 2:
+        logger.warning("No URL provided")
         return
 
     url = sys.argv[1]
     profile = choose_profile(url)
+
+    logger.info(f"Opening {url} with profile: {profile}")
     open_in_chrome(url, profile)
 
 if __name__ == "__main__":
